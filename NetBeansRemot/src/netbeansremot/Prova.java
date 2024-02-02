23c127b9c40eb45e5e956c030ccc0fc690061f8f
Oriol Trullols <orioltrullolsfabregat@gmail.com>
2024-02-02 11:42:35 +0100

    Primer canvi al repositori remot

---
+++ NetBeansRemot/src/netbeansremot/Prova.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package netbeansremot;
+
+/**
+ *
+ * @author otrullols
+ */
+public class Prova {
+        public static void main(String[] args) {
+        System.out.println("Treballem amb Java en NetBeans i ara amb GITHub");
+    }
+}
